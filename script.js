28296ebd01aeceac6374843eedaa347148ddfb5a
Marc M <marcmwebdev@gmail.com>
2021-10-24 21:17:01 -0400

    answer 2

--- script.js
+++ script.js
@@ -4,12 +4,41 @@
 
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
+    let operation = document.getElementById('operation').value;
 
-    let sum = parseFloat(num1 + num2);
+   let answer = 0;
 
-    document.getElementById('answer').value = sum;
+
+    switch(operation) {
+      case 'add':
+        answer = parseFloat(num1 + num2);
+      break;
+      case 'subtract':
+        answer = parseFloat(num1 - num2);
+      break;
+      case 'multiply':
+        answer = parseFloat(num1 * num2);
+      break;
+      case 'divide':
+        answer = parseFloat(num1 / num2);
+      break;
+    }
+
+
+
+    document.getElementById('answer').value = answer;
 });
 
+
+
+
+
+
+
+ 
+
+
+ 
 
 
 
